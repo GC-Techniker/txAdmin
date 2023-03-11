c12b07a13305de45de0cae93d7233353eb6880e9
Patrick Budelmann <68276540+PatrickPurzel@users.noreply.github.com>
2023-03-11 21:39:49 +0100

    message

--- .deploy.config.js
+++ .deploy.config.js
@@ -1,24 +1,30 @@
 export default {
-    fxserverPath: 'E:\\FiveM\\BUILDS\\6185\\fxserver.exe',
-    debouncerInterval: 250,
-    preReleaseExpirationDays: 21,
-    txAdminArgs: [
-        '+set', 'txAdminDevMode', 'true',
-        '+set', 'txAdminVerbose', 'true',
-        // '+set', 'txAdminPort', '40121',
-        // '--trace-warnings',
-        
-        //FIXME: broken
-        // '+set', 'txDebugPlayerlistGenerator', 'true',
-        // '+set', 'txDebugExternalSource', 'xxxxxxxx:30120',
-    ],
-    copy: [
-        'README.md',
-        'LICENSE',
-        'docs/',
-        'fxmanifest.lua',
-        'entrypoint.js',
-        'resource/',
-        'web/',
-    ],
-}
+  fxserverPath: "D:\\FXserver\\server\\FXserver.exe",
+  debouncerInterval: 250,
+  preReleaseExpirationDays: 21,
+  txAdminArgs: [
+    "+set",
+    "txAdminDevMode",
+    "true",
+    "+set",
+    "txAdminVerbose",
+    "true",
+    "+set",
+    "txAdminPort",
+    "40121",
+    // '--trace-warnings',
+
+    //FIXME: broken
+    // '+set', 'txDebugPlayerlistGenerator', 'true',
+    // '+set', 'txDebugExternalSource', 'xxxxxxxx:30120',
+  ],
+  copy: [
+    "README.md",
+    "LICENSE",
+    "docs/",
+    "fxmanifest.lua",
+    "entrypoint.js",
+    "resource/",
+    "web/",
+  ],
+};
